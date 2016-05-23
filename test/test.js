89351a7f38b6c9120e65440b24048885007c6629
Nick Meldrum <nick@nickmeldrum.com>
2016-05-23 21:44:40 +0100

    sets up 1st feature spec

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var chai = require('chai')
+var expect = chai.expect
+
+describe('Feature: Calculate totals', function() {
+    describe('Given the basket has 1 bread, 1 butter and 1 milk when I total the basket', function() {
+        it.skip('then the total should be £2.95', function() {
+        })
+    })
+})
