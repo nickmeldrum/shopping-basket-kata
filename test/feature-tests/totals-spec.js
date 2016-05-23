90d5221d331548546469533f86463fafdfd4436e
Nick Meldrum <nick@nickmeldrum.com>
2016-05-24 00:05:29 +0100

    refactors totals feature to use product lookup

--- test/feature-tests/totals-spec.js
+++ test/feature-tests/totals-spec.js
@@ -1,4 +1,5 @@
 const basketCreator = require('../../lib/basket')
+const productLookup = require('../../lib/product')
 const chai = require('chai')
 const expect = chai.expect
 
@@ -6,9 +7,9 @@ describe('Feature: Calculate totals', function() {
     describe('Given the basket has 1 bread, 1 butter and 1 milk when I total the basket', function() {
         it('then the total should be £2.95', function() {
             const basket = basketCreator()
-            basket.add({name: 'Bread', cost: 1.00})
-            basket.add({name: 'Butter', cost: 0.80})
-            basket.add({name: 'Milk', cost: 1.15})
+            basket.add(productLookup('bread'))
+            basket.add(productLookup('butter'))
+            basket.add(productLookup('milk'))
             
             expect(basket.getTotal()).to.equal(2.95)
         })
