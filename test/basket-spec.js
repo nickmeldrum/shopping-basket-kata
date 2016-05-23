34cc749bee7c482a5c0128beabf5befdc57a56f7
Nick Meldrum <nick@nickmeldrum.com>
2016-05-23 23:10:09 +0100

    reorganises tests

--- test/basket-spec.js
+++ test/basket-spec.js
@@ -3,35 +3,39 @@ const expect = chai.expect
 const basket = require('../lib/basket')
 
 describe('shopping basket', function() {
-    it('empty basket returns a total of zero', function() {
-        expect(basket.getTotal()).to.equal(0)
+    context('totals', function() {
+        it('empty basket returns a total of zero', function() {
+            expect(basket.getTotal()).to.equal(0)
+        })
     })
 
-    it('add undefined throws exception', function() {
-        expect(() => basket.add()).to.throw()
-    })
+    context('defensive tests', function() {
+        it('add undefined throws exception', function() {
+            expect(() => basket.add()).to.throw()
+        })
 
-    it('add null throws exception', function() {
-        expect(() => basket.add(null)).to.throw()
-    })
+        it('add null throws exception', function() {
+            expect(() => basket.add(null)).to.throw()
+        })
 
-    it('product without cost throws exception', function() {
-        expect(() => basket.add({})).to.throw()
-    })
+        it('product without cost throws exception', function() {
+            expect(() => basket.add({})).to.throw()
+        })
 
-    it('cost not a number throws exception', function() {
-        expect(() => basket.add({cost: 'a string'})).to.throw()
-    })
+        it('cost not a number throws exception', function() {
+            expect(() => basket.add({cost: 'a string'})).to.throw()
+        })
 
-    it('product has no name throws exception', function() {
-        expect(() => basket.add({cost: 1})).to.throw()
-    })
+        it('product has no name throws exception', function() {
+            expect(() => basket.add({cost: 1})).to.throw()
+        })
 
-    it('product name not a string throws exception', function() {
-        expect(() => basket.add({name: 1, cost: 1})).to.throw()
-    })
+        it('product name not a string throws exception', function() {
+            expect(() => basket.add({name: 1, cost: 1})).to.throw()
+        })
 
-    it('product with string name and numeric cost does not throw', function() {
-        expect(() => basket.add({name: 'name', cost: 1})).to.not.throw()
+        it('product with string name and numeric cost does not throw', function() {
+            expect(() => basket.add({name: 'name', cost: 1})).to.not.throw()
+        })
     })
 })
