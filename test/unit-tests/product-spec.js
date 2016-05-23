2033fb5293757e6e5f18a3653ac58e6d7158d498
Nick Meldrum <nick@nickmeldrum.com>
2016-05-23 23:55:12 +0100

    butter product lookup test

--- test/unit-tests/product-spec.js
+++ test/unit-tests/product-spec.js
@@ -1,10 +1,15 @@
 const chai = require('chai')
 const expect = chai.expect
-const product = require('../../lib/product')
+const lookup = require('../../lib/product')
 
 describe('products', function() {
     it('looks up bread by name and gets product that costs £1.00', function() {
-        const bread = product('bread')
-        expect(bread.cost).to.equal(1.00)
+        const product = lookup('bread')
+        expect(product.cost).to.equal(1.00)
+    })
+
+    it('looks up butter by name and gets product that costs £0.80', function() {
+        const product = lookup('butter')
+        expect(product.cost).to.equal(0.80)
     })
 })
