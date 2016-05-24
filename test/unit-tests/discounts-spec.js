00fefde2c1c1789444e1b895b05f9c16c8f2eb93
Nick Meldrum <nick@nickmeldrum.com>
2016-05-24 06:15:00 +0100

    discount on product with requirement on another product test

--- test/unit-tests/discounts-spec.js
+++ test/unit-tests/discounts-spec.js
@@ -5,16 +5,62 @@ const basketCreator = require('../../lib/basket')
 describe('shopping basket', function() {
     context('discounts', function() {
         it('apply a simple discount of 50% off milk to a basket of 1 milk and totals = half the milk cost', function() {
-            basket = basketCreator([{name: 'milk', discount: 0.5}])
+            basket = basketCreator([
+                {
+                    requirement: {
+                        name: 'milk'
+                    },
+                    product: {
+                        name: 'milk',
+                        discount: 0.5
+                    }
+                }
+            ])
             basket.add({name: 'milk', cost: 1})
             expect(basket.getTotal()).to.equal(0.5)
         })
 
         it('apply 2 discounts, 50% off milk and 50% off bread and total is half off', function() {
-            basket = basketCreator([{name: 'milk', discount: 0.5},{name: 'bread', discount: 0.5}])
+            basket = basketCreator([
+                {
+                    requirement: {
+                        name: 'milk'
+                    },
+                    product: {
+                        name: 'milk',
+                        discount: 0.5
+                    }
+                },
+                {
+                    requirement: {
+                        name: 'bread'
+                    },
+                    product: {
+                        name: 'bread',
+                        discount: 0.5
+                    }
+                }
+            ])
             basket.add({name: 'milk', cost: 1.6})
             basket.add({name: 'bread', cost: 1.2})
             expect(basket.getTotal()).to.equal(1.4)
+        })
+
+        it('require bread to be in basket to get 50% off all milk', function() {
+            basket = basketCreator([
+                {
+                    requirement: {
+                        name: 'bread'
+                    },
+                    product: {
+                        name: 'milk',
+                        discount: 0.5
+                    }
+                }
+            ])
+            basket.add({name: 'milk', cost: 1.6})
+            basket.add({name: 'bread', cost: 1.2})
+            expect(basket.getTotal()).to.equal(2.0)
         })
     })
 })
