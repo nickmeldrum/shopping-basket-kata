ca5425f811cb9b32df7a0100f290438e036ce3f3
Nick Meldrum <nick@nickmeldrum.com>
2016-05-24 18:09:17 +0100

    renames products get func and vars

--- test/feature-tests/total-spec.js
+++ test/feature-tests/total-spec.js
@@ -11,9 +11,9 @@ describe('Feature: Calculate totals', function() {
     describe('Given the basket has 1 bread, 1 butter and 1 milk when I total the basket', function() {
         it('then the total should be £2.95', function() {
             const basket = basketFactory()
-            basket.add(product.products('bread'))
-            basket.add(product.products('butter'))
-            basket.add(product.products('milk'))
+            basket.add(product.get('bread'))
+            basket.add(product.get('butter'))
+            basket.add(product.get('milk'))
             
             expect(basket.getTotal()).to.equal(2.95)
         })
@@ -22,8 +22,8 @@ describe('Feature: Calculate totals', function() {
     describe('Given the basket has 2 butter and 2 bread when I total the basket', function() {
         it('then the total should be £3.10', function() {
             const basket = basketFactory()
-            basket.add(product.products('bread'), 2)
-            basket.add(product.products('butter'), 2)
+            basket.add(product.get('bread'), 2)
+            basket.add(product.get('butter'), 2)
             
             expect(basket.getTotal(discount.discounts)).to.equal(3.10)
         })
@@ -32,7 +32,7 @@ describe('Feature: Calculate totals', function() {
     describe('Given the basket has 4 milk when I total the basket', function() {
         it('then the total should be £3.45', function() {
             const basket = basketFactory()
-            basket.add(product.products('milk'), 4)
+            basket.add(product.get('milk'), 4)
             
             expect(basket.getTotal(discount.discounts)).to.equal(3.45)
         })
@@ -41,7 +41,7 @@ describe('Feature: Calculate totals', function() {
     describe('Given the basket has 2 butter, 1 bread and 8 milk when I total the basket', function() {
         it('then the total should be £9.00', function() {
             const basket = basketFactory()
-            basket.add(product.products('milk'), 4)
+            basket.add(product.get('milk'), 4)
             
             expect(basket.getTotal(discount.discounts)).to.equal(3.45)
         })
--- test/unit-tests/product-spec.js
+++ test/unit-tests/product-spec.js
@@ -2,26 +2,26 @@
 
 const chai = require('chai')
 const expect = chai.expect
-const product = require('../../lib/product')
+const products = require('../../lib/product')
 
 describe('products', function() {
     it('looks up bread by name and gets product that costs £1.00', function() {
-        const prod = product.products('bread')
-        expect(prod.cost).to.equal(1.00)
+        const product = products.get('bread')
+        expect(product.cost).to.equal(1.00)
     })
 
     it('looks up butter by name and gets product that costs £0.80', function() {
-        const prod = product.products('butter')
-        expect(prod.cost).to.equal(0.80)
+        const product = products.get('butter')
+        expect(product.cost).to.equal(0.80)
     })
 
     it('looks up milk by name and gets product that costs £1.15', function() {
-        const prod = product.products('milk')
-        expect(prod.cost).to.equal(1.15)
+        const product = products.get('milk')
+        expect(product.cost).to.equal(1.15)
     })
 
     it('looks up Milk with uppercase and gets milk product', function() {
-        const prod = product.products('Milk')
-        expect(prod.cost).to.equal(1.15)
+        const product = products.get('Milk')
+        expect(product.cost).to.equal(1.15)
     })
 })
