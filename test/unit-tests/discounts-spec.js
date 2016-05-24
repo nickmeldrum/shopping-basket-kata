2f3343a14c23d8219bb1fe04df8a89b59f034da7
Nick Meldrum <nick@nickmeldrum.com>
2016-05-24 05:59:35 +0100

    2 50% off discounts test

--- test/unit-tests/discounts-spec.js
+++ test/unit-tests/discounts-spec.js
@@ -5,9 +5,16 @@ const basketCreator = require('../../lib/basket')
 describe('shopping basket', function() {
     context('discounts', function() {
         it('apply a simple discount of 50% off milk to a basket of 1 milk and totals = half the milk cost', function() {
-            basket = basketCreator({name: 'milk', discount: 0.5})
+            basket = basketCreator([{name: 'milk', discount: 0.5}])
             basket.add({name: 'milk', cost: 1})
             expect(basket.getTotal()).to.equal(0.5)
+        })
+
+        it('apply 2 discounts, 50% off milk and 50% off bread and total is half off', function() {
+            basket = basketCreator([{name: 'milk', discount: 0.5},{name: 'bread', discount: 0.5}])
+            basket.add({name: 'milk', cost: 1.6})
+            basket.add({name: 'bread', cost: 1.2})
+            expect(basket.getTotal()).to.equal(1.4)
         })
     })
 })
