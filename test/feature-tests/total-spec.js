7e548d132a68330e34c7b394155d756341645ac7
Nick Meldrum <nick@nickmeldrum.com>
2016-05-24 15:15:33 +0100

    rename modules refactor

--- test/feature-tests/total-spec.js
+++ test/feature-tests/total-spec.js
@@ -3,17 +3,17 @@
 const chai = require('chai')
 const expect = chai.expect
 
-const productLookup = require('../../lib/product')
-const discountLookup = require('../../lib/discount-lookup')
+const product = require('../../lib/product')
+const discount = require('../../lib/discount')
 const basketCreator = require('../../lib/basket')
 
 describe('Feature: Calculate totals', function() {
     describe('Given the basket has 1 bread, 1 butter and 1 milk when I total the basket', function() {
         it('then the total should be £2.95', function() {
             const basket = basketCreator()
-            basket.add(productLookup('bread'))
-            basket.add(productLookup('butter'))
-            basket.add(productLookup('milk'))
+            basket.add(product.products('bread'))
+            basket.add(product.products('butter'))
+            basket.add(product.products('milk'))
             
             expect(basket.getTotal()).to.equal(2.95)
         })
@@ -22,28 +22,28 @@ describe('Feature: Calculate totals', function() {
     describe('Given the basket has 2 butter and 2 bread when I total the basket', function() {
         it('then the total should be £3.10', function() {
             const basket = basketCreator()
-            basket.add(productLookup('bread'), 2)
-            basket.add(productLookup('butter'), 2)
+            basket.add(product.products('bread'), 2)
+            basket.add(product.products('butter'), 2)
             
-            expect(basket.getTotal(discountLookup)).to.equal(3.10)
+            expect(basket.getTotal(discount.discounts)).to.equal(3.10)
         })
     })
 
     describe('Given the basket has 4 milk when I total the basket', function() {
         it('then the total should be £3.45', function() {
             const basket = basketCreator()
-            basket.add(productLookup('milk'), 4)
+            basket.add(product.products('milk'), 4)
             
-            expect(basket.getTotal(discountLookup)).to.equal(3.45)
+            expect(basket.getTotal(discount.discounts)).to.equal(3.45)
         })
     })
 
     describe('Given the basket has 2 butter, 1 bread and 8 milk when I total the basket', function() {
         it('then the total should be £9.00', function() {
             const basket = basketCreator()
-            basket.add(productLookup('milk'), 4)
+            basket.add(product.products('milk'), 4)
             
-            expect(basket.getTotal(discountLookup)).to.equal(3.45)
+            expect(basket.getTotal(discount.discounts)).to.equal(3.45)
         })
     })
 })
--- test/unit-tests/product-spec.js
+++ test/unit-tests/product-spec.js
@@ -2,26 +2,26 @@
 
 const chai = require('chai')
 const expect = chai.expect
-const lookup = require('../../lib/product')
+const product = require('../../lib/product')
 
 describe('products', function() {
     it('looks up bread by name and gets product that costs £1.00', function() {
-        const product = lookup('bread')
-        expect(product.cost).to.equal(1.00)
+        const prod = product.products('bread')
+        expect(prod.cost).to.equal(1.00)
     })
 
     it('looks up butter by name and gets product that costs £0.80', function() {
-        const product = lookup('butter')
-        expect(product.cost).to.equal(0.80)
+        const prod = product.products('butter')
+        expect(prod.cost).to.equal(0.80)
     })
 
     it('looks up milk by name and gets product that costs £1.15', function() {
-        const product = lookup('milk')
-        expect(product.cost).to.equal(1.15)
+        const prod = product.products('milk')
+        expect(prod.cost).to.equal(1.15)
     })
 
     it('looks up Milk with uppercase and gets milk product', function() {
-        const product = lookup('Milk')
-        expect(product.cost).to.equal(1.15)
+        const prod = product.products('Milk')
+        expect(prod.cost).to.equal(1.15)
     })
 })
